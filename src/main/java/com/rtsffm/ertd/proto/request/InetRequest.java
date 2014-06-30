6bcbb6ba1f14b0bb256e2c7cab82ccc997530a73
Pavan Kumar Mankala <pavanmankala@askme.com>
2014-06-30 14:03:11 +0200

    delete model

--- src/main/java/com/rtsffm/ertd/proto/request/InetRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rtsffm.ertd.proto.request;
-
-import com.rtsffm.ertd.inetcom.Rid;
-
-//~--- interfaces -------------------------------------------------------------
-
-public interface InetRequest extends InetStruct {
-    Rid getRid();
-}
